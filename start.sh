f9ee5b4513d1c3df03b36c89839193243c5086d4
jay-choe <jay0764@naver.com>
2022-08-07 21:09:26 +0900

    add redis docker

--- start.sh
+++ start.sh
@@ -1,8 +1,19 @@
 # Run dev image
 set -xe
 
+echo "Starting keycloak server container...."
+
 docker run -p 28080:8080 \
  -d \
+ --name keycloak \
  -e KEYCLOAK_ADMIN=admin \
  -e KEYCLOAK_ADMIN_PASSWORD=admin \
  quay.io/keycloak/keycloak:19.0.1 start-dev
+
+ echo "Starting Redis Container...."
+
+ docker run -d \
+ --name redis \
+ -p 6379:6379 redis
+
+# TODO: mig to docker-compose
