27dabc5d0676dec6f2643bf3cf0d8ad421fd75ae
jay-choe <jay0764@naver.com>
2022-08-13 23:49:29 +0900

    rm unnessesary import

--- src/main/java/com/keycloak/demo/configurations/web/WebConfigurer.java
+++ src/main/java/com/keycloak/demo/configurations/web/WebConfigurer.java
@@ -1,7 +1,5 @@
 package com.keycloak.demo.configurations.web;
 
-import java.time.Duration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
